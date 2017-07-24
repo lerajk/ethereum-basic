a2b8f7bc6063af4ce00aaff94e31afafe1864394
lerajk <lerajk44@gmail.com>
2017-07-24 16:16:39 -0400

    intitial commit

---
+++ greeter.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.4.0;
+
+contract mortal { 
+    
+    address owner;
+    
+    function mortal(){
+        owner = msg.sender;
+    }
+    
+    function kill(){
+        
+        if(msg.sender == owner){
+            
+            selfdestruct(owner);
+        }
+    } //function kill ends 
+    
+} //contract end 
+
+contract greeter is mortal {
+    
+    string greeting;
+    
+    function greeter (string _greeting) public {
+        
+        greeting = _greeting;
+    }
+    
+    function greet() constant returns (string){
+        return greeting; 
+    }
+    
+  
+} // greeter is mortal ends
